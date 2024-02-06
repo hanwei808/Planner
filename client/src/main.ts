ebd7534f99132ee0b9b87255223c9420ad6e9c45
hanwei808 <hancenter808@outlook.com>
2024-02-06 18:02:21 +0800

    首页增加SVG展示

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,9 +4,11 @@ import router from './router'
 import { store, key } from './store'
 import './styles/index.scss'
 import elementPlus from './plugins/element-plus'
+import InlineSvg from 'vue-inline-svg'
 
 // @ts-expect-error: 暂时关闭router提示
 createApp(App).use(router)
     .use(store, key)
     .use(elementPlus)
+    .component('inline-svg', InlineSvg)
     .mount('#app')
