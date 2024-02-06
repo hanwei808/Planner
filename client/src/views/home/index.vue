d0f42f32598881174db1d1a4c2477b7c1e9f1017
hanwei808 <hancenter808@outlook.com>
2024-02-06 18:52:15 +0800

    提交

--- client/src/views/home/index.vue
+++ client/src/views/home/index.vue
@@ -1,31 +1,89 @@
 <template>
-  <div class="home">
-    <div
-      v-for="(url, path) in svgs"
-      :key="path"
-    >
-      {{ typeof path === 'string' ? path.split('/')?.pop() : '' }}
-      <inline-svg :src="url" />
+  <div
+    class="article-container"
+    :class="{'md-sidebar-expand': !$store.state.isMdCollapse}"
+  >
+    <div class="home">
+      <div
+        v-for="(url, path) in svgs"
+        :key="path"
+      >
+        <h2
+          :id="typeof path === 'string' ? path.split('/')?.pop()?.split('.')?.shift() : ''"
+          ebook-toc-level-1=""
+          :heading="typeof path === 'string' ? path.split('/')?.pop()?.split('.')?.shift() : ''"
+        >
+          {{ typeof path === 'string' ? path.split('/')?.pop()?.split('.')?.shift() : '' }}
+        </h2>
+        <inline-svg :src="url" />
+      </div>
     </div>
+    <mdSidebar :navigation="navigation" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import mdSidebar from '@/components/mdSidebar.vue';
+import { ref, Ref, onMounted } from 'vue';
 
 const svgFiles = import.meta.glob('./drawio/*.svg')
 
 let svgs = ref<{ [key: string]: string }>({});
+
+// Define a type for navigation items
+type NavigationItem = {
+  title: string;
+  id: string;
+  children: NavigationItem[];
+};
+
+// Define the type for the navigation ref
+const navigation: Ref<NavigationItem[]> = ref([]);
 
 onMounted(async () => {
   const icons = Object.keys(svgFiles).map(async (path) => {
     svgs.value[path] = new URL(path, import.meta.url).href;
   })
   await Promise.all(icons);
+  createNavigation();
 });
+
+const createNavigation = (): void => {
+  const headers = Array.from(document.querySelectorAll('h2, h3')) as HTMLElement[];
+  let currentH1: NavigationItem | null = null;
+
+  headers.forEach(header => {
+    const newItem: NavigationItem = {
+      title: header.innerText,
+      id: header.id,
+      children: []
+    };
+
+    if (header.tagName === 'H2') {
+      currentH1 = newItem;
+      navigation.value.push(newItem);
+    } else if (header.tagName === 'H3' && currentH1) {
+      currentH1.children.push(newItem);
+    }
+  });
+};
 </script>
 
 <style scoped>
+.article-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 100%;
+}
+@media screen and (min-width: 600px) {
+  .article-container {
+    padding-right: 50px;
+  }
+  .md-sidebar-expand {
+    padding-right: 200px;
+  }
+}
 .home {
   div {
     display: flex;
